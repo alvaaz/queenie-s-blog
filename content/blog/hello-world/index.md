c3daf8110527313af214ad81a15d36ed7d4fb581
Álvaro <agoede@jumpitt.com>
2019-07-24 12:14:01 -0400

    :fire: change conf

--- content/blog/hello-world/index.md
+++ content/blog/hello-world/index.md
@@ -1,9 +1,9 @@
 ---
 title: Hello World
-date: "2015-05-01T22:12:03.284Z"
+date: "2019-07-01T22:12:03.284Z"
 ---
 
-This is my first post on my new fake blog! How exciting!
+Este es mi primer artículo!
 
 I'm sure I'll write a lot more interesting things in the future.
 
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Queenie's Blog`,
     author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    description: `Bienvenido a mi blog de comida.`,
+    siteUrl: `https://queenie.cl/`,
     social: {
-      twitter: `kylemathews`,
+      instagram: `queeniecmoya`,
     },
   },
   plugins: [
@@ -57,8 +57,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Queenie's Blog`,
+        short_name: `Queenie`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
